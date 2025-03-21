69a6477235b28a5af1be1bc5588c58fa37875be8
Philip Calub <philip@matterdesign.com.au>
2025-03-21 23:05:30 +0800

    Card Template Page and Route

--- src/pages/cardTemplates/components/CreateCardTemplate.jsx
+++ src/pages/cardTemplates/components/CreateCardTemplate.jsx
@@ -1,33 +1,56 @@
-import { Dialog, DialogActions, DialogContent, DialogTitle, Divider } from "@mui/material";
+import { Dialog, DialogActions, DialogContent, DialogTitle, Divider, IconButton } from "@mui/material";
 import { useEffect, useState } from "react";
 import axios from 'axios';
-import { GetActiveInstitution } from "../../../global/Helpers";
+import { axiosErrorCodeHandler, GetActiveInstitution } from "../../../global/Helpers";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useAlert } from "../../../hooks/CustomHooks";
 
 export default function CreateCardTemplate({refresh}){
     const [open, setOpen] = useState(false);
     const [subjects, setSubjects] = useState([]);
     const [newSubject, setNewSubject] = useState("");
+    const [newSubjectMatch, setNewSubjectMatch] = useState("");
     const [isEmpty, setIsEmpty] = useState(false);
     const [templateTitle, setTemplateTitle] = useState("");
     const [submitting, setSubmitting] = useState(false);
     const institution = GetActiveInstitution();
+    const alert = useAlert();
     
     const handleOpenModal = () => {
         setOpen(!open);
         setSubjects([]);
     };
-    
+
     const handleAddSubject = () => {
         if(newSubject == ''){
             setIsEmpty(true);
             return
         }
-        setSubjects(prevState => [...prevState, {title: newSubject, slug: String(newSubject).replaceAll(" ", "-").toLowerCase()}]);
+        setSubjects(prevState => [...prevState,
+            {
+                card_subject: newSubject,
+                subject_to_match: newSubjectMatch,
+                slug: newSubject.replaceAll(" ", "-").toLowerCase()
+            }]);
         setNewSubject("");
+        setNewSubjectMatch("");
     };
-    
+
     const handleRemoveSubject = (slug) => {
         setSubjects(prevState => prevState.filter(subject => subject.slug != slug));
+    };
+
+    const handleAdjustSubjectPosition = (currentIndex, direction) => {
+        setSubjects(prevSubjects => {
+            const newIndex = currentIndex + direction;
+            const newSubjects = [...prevSubjects];
+            const elementToMove = newSubjects.splice(currentIndex, 1)[0];
+            newSubjects.splice(newIndex, 0, elementToMove);
+
+        return newSubjects;
+        });
     };
     
     const handleSubmit = async () => {
@@ -41,9 +64,10 @@ export default function CreateCardTemplate({refresh}){
         .then(() => {
             handleOpenModal();
             refresh();
+            alert.setAlert('success', 'Template Created');
         })
-        .catch(() => {
-            
+        .catch(err => {
+            alert.setAlert('success', axiosErrorCodeHandler(err));
         })
         .finally(() => {
             setSubmitting(false);
@@ -65,28 +89,37 @@ export default function CreateCardTemplate({refresh}){
             <DialogTitle className="fw-bolder">Create New Template</DialogTitle>
             <Divider />
             <DialogContent>
-                <div className="d-flex flex-column gap-3">
-                    <div className="d-flex flex-row gap-2">
+                <div className="d-flex flex-column">
+                    <div className="d-flex flex-row gap-2 mb-5">
                         <input type="text" className={`form-control`} placeholder="Template Title/Description Eg.(Card Template for G8)" value={templateTitle} onChange={(e) => setTemplateTitle(e.target.value)}/>
                     </div>
-                    <div className="d-flex flex-row gap-2 align-items-start">
-                        <div className="d-flex flex-column w-100">
-                            <input type="text" className={`form-control ${isEmpty ? 'border border-danger' : ''}`} placeholder="Subject Title" value={newSubject} onChange={(e) => setNewSubject(e.target.value)}/>
-                            <p className="fw-light fst-italic text-secondary" style={{fontSize: '12px'}}>Note: always check the spelling.</p>
-                        </div>
-                        <button className="btn btn-sm btn-primary" onClick={() => handleAddSubject()}>Add</button>
+                    <div className="d-flex flex-column gap-2 align-items-start">
+                    <input type="text" className={`form-control ${isEmpty ? 'border border-danger' : ''}`} placeholder="Subject that will show on card" value={newSubject} onChange={(e) => setNewSubject(e.target.value)}/>
+                    <input type="text" className={`form-control ${isEmpty ? 'border border-danger' : ''}`} placeholder="Subject that will match the teacher subject" value={newSubjectMatch} onChange={(e) => setNewSubjectMatch(e.target.value)}/>
+                    <button className="btn btn-sm btn-primary w-100" onClick={() => handleAddSubject()}>Add</button>
+                    <p className="m-0 text-muted fst-italic">Note: Always check for spelling, 2nd Input must be same with what the teacher encoded.</p>
                     </div>
+                    <hr />
                     <div className="mt-3 d-flex flex-column align-items-center">
                         {subjects.length == 0 && (
-                            <p className="fw-bolder">ADD SOME SUBJECTS</p>
+                            <p className="fw-bolder">NO SUBJECTS</p>
                         )}
                         {subjects.map((subject, i) => (
-                            <div key={i} className="py-1 d-flex flex-row gap-2 w-100">
-                                <input type="text" className="form-control" value={subject.title} disabled/>
-                                <button className="btn btn-sm btn-danger" onClick={() => handleRemoveSubject(subject.slug)}>X</button>
+                            <div key={i} className="py-1 d-flex flex-row gap-2 w-100 align-items-center">
+                                <input type="text" className="form-control" defaultValue={`${subject.card_subject} (${subject.subject_to_match})`} disabled/>
+                                <IconButton color="primary" size="small" onClick={() => handleAdjustSubjectPosition(i, -1)} disabled={i == 0}>
+                                    <KeyboardArrowUpIcon fontSize="inherit"/>
+                                </IconButton>
+                                <IconButton color="primary" size="small" onClick={() => handleAdjustSubjectPosition(i, +1)} disabled={i == subjects.length - 1}>
+                                    <KeyboardArrowDownIcon fontSize="inherit"/>
+                                </IconButton>
+                                <IconButton color="error" size="small" onClick={() => handleRemoveSubject(subject.slug)}>
+                                    <DeleteIcon fontSize="inherit"/>
+                                </IconButton>
                             </div>
                         ))}
                     </div>
+                    <hr />
                 </div>
             </DialogContent>
             <DialogActions className="d-flex flex-row justify-content-start">
